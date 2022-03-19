38ddb43ae20a6a76dd33535444b18444530383a7
Ailton Rafael <ailton.junior@al.infnet.edu.br>
2022-03-19 01:16:26 -0300

    Projeto 2

--- Semana 2/Aula 1/index.js
+++ Semana 2/Aula 1/index.js
@@ -1,51 +1,72 @@
-// Importando o módulo o express
+// Importando o moódulo o express
 import express from 'express'
-
-// Importando o path, que serve para informar caminhos para o EJS
+// Serve para servir documentos
 import path from 'path'
 
-// Informa a pasta raiz
-const __dirname = path.resolve(path.dirname(''))
+let message = ''
+
+// Importar Body Parser
+
+// Importar json schema
+import { validate } from 'express-jsonschema'
 
 // Criar uma variável e instanciar (ou seja: passar todos as funções da função express para o variável app)
 const app = express()
 // Informa para o meu express que o motor de visualização (ou seja, o modo que vai exibir as minhas páginas) vai ser o EJS
 app.set('view engine', 'ejs')
-
-// Informa onde estão os arquivos estáticos (que são todos os arquivos complementares do HMTL/EJS - EX: CSS, JS, IMG e etc)
-app.use(express.static(path.join(__dirname, "public")))
-
-// Serve para 'parsear' - ou seja, transformar em JSON - a requisição do usário
+// Informar o express qual biblioteca fazer o parser - true usa biblioteca QS - query string
 app.use(express.urlencoded({extended: true}))
-// Serve para que a informação que vem do body da requisição - ou seja, informação que usário nos envia - vire JSON
 app.use(express.json())
 
-// Variável porta = indica qual porta o nosso servidor vai rodar /  (3000 - 4999)
+// Informar caminho padrão
+let __dirname = path.resolve(path.dirname(''));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Variável porta = indica qual porta o nosso servidor vai rodar
 const port = 3001
 
-// Onde roda o servidor - Listen serve para rodar o servidor
-app.listen(port, ()=>{
-    console.log(`Estou rodando dentro da port ${port}`)
+const formSchema = {
+    type: 'array',
+    properties: {
+        name: {
+            type: 'string',
+            required: true
+        },
+        email: {
+            type: 'string',
+            required: true
+        }
+    }
+}
+
+app.listen(port, () => {
+console.log(`Meu servidor está rodando na porta ${port}`)
 })
 
-// **********************************************************************
+app.get("/", (req, res) => {
+    const devList = ["Backend", "Frontend", "Fullstack"];
+    const analyticsList = ["Engenharia de dados", "Ciência de dados"];
 
-app.get('/', (req, res) =>{
-    const cursos = ['FullStack', 'Java', 'CSS', 'HTML', 'Python', 'PHP', 'SQL']
-    res.render('index.ejs', {
-        cursos,
-        mensagem
+    setTimeout(() => {
+        message = "";
+      }, 1000);
+      
+    res.render("index.ejs", {
+        nome: "Sexta-feira",
+        devList,
+        analyticsList,
+        message
     })
 })
 
-app.get('/sobre', (req, res)=>{
-    res.render('sobre.ejs')
+app.post("/subscription", (req, res) => {
+    const { nome, email } = req.body
+    // res.send({ nome, email } )
+    message = `Parabéns ${nome}, sua inscrição foi realizada com sucesso!
+    um email foi envadiado para ${email}`
+    res.redirect('/')
 })
 
-let mensagem = ''
+app.get("/sobre", validate({body: formSchema}), (req, res) => {
 
-app.post('/', (req, res) => {
-    const { nome, email } = req.body
-    mensagem = `Olá, sr(a) ${nome}! Enviamos um email para seu ${email}`
-    res.redirect('/')
 })
---
+++ Semana 2/Aula 1/public/js/index.js
@@ -0,0 +1,6 @@
+const closeMessage = document.querySelector("#close")
+const message = document.querySelector("#message")
+
+closeMessage.addEventListener("click", function (){
+    message.style.display = "none"
+})
